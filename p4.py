83b5307465eeb01894e51591b1d6519456b4292e
FilipeGomesMelo <fgm3@cin.ufpe.br>
2021-12-14 10:37:19 -0300

    Update p4.py

--- p4.py
+++ p4.py
@@ -1,13 +1,25 @@
 from tkinter import *
+from datetime import datetime
+import socket
 
 class GUI:
-    def __init__(self, width, height) -> None:
+    def __init__(self, width, height, name, target) -> None:
+        self.name = name
         self.window = Tk()
-        self.window.title("P4")
+        self.window.title("waiting...")
 
         self.canva = Canvas(self.window, width=width, height=height)
         self.canva.grid(columnspan=3)
+        
+        self.target_ip = target
+
         self.createWidgets()
+
+        self.status = ''
+        self.conn = None
+        self.addr = None
+        self.connector = None
+        self.connect()
     
     def createWidgets(self):
         self.txt_area = Text(self.canva, border=1)
@@ -15,19 +27,52 @@ class GUI:
         self.send_button = Button(self.canva, text='Send', padx=40, command=self.send)
 
         self.window.bind('<Return>', self.send)
+        self.window.bind('<Shift_R>', self.update)
         self.txt_area.config(background='#c8a2c8')
 
         self.txt_area.grid(column=0, row=0, columnspan=3)
         self.txt_field.grid(column=0, row=1, columnspan=2)
         self.send_button.grid(column=2, row=1)
 
+    def connect(self):
+        try:
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s.connect((self.target_ip, 28886))
+            self.status = f'{self.name} - client'
+            self.connector = self.s
+        except ConnectionRefusedError:
+            self.status = f'{self.name} - Server'
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s.bind(('localhost', 28886))
+            self.s.listen(1)
+            self.conn, self.addr = self.s.accept()
+            self.connector = self.conn
+        finally:
+            self.window.title(self.status)
+    
     def send(self, event=None):
-        texto = self.txt_field.get()
-        self.txt_area.insert(END, texto)
+        msg = self.txt_field.get()
+        msg = msg.replace("\\n","\n")
+        msg = f"\n{self.name}: {msg}\n{datetime.now().strftime('%d/%m/%Y, %H:%M:%S')}\n"
+        self.connector.sendall(bytes(msg, 'utf-8')) 
+        self.txt_area.insert(END, msg)
         self.txt_field.delete(0, END)
+
+    def update(self, event=None):
+        try:
+            while True:
+                self.connector.settimeout(0.001)
+                msg = self.connector.recv(1024)
+                if not msg:
+                    return
+                msg = msg.decode("utf-8").replace("\\n","\n")
+                self.txt_area.insert(END, msg)
+        except TimeoutError:
+            print("Todas mensagens foram carregadas")
 
     def start(self):
         self.window.mainloop()
 
 if __name__ == '__main__':
-    interface = GUI(600,800).start()
+    nome = input("Qual o seu nome? ")
+    interface = GUI(600, 800, nome, 'localhost').start()
