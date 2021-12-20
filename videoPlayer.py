77da36fb1abdb58da28726bec3836f014907b873
FilipeGomesMelo <61971614+FilipeGomesMelo@users.noreply.github.com>
2021-12-20 18:08:05 -0300

    Create videoPlayer.py

---
+++ videoPlayer.py
@@ -0,0 +1,27 @@
+import tkinter as tk, threading
+import time
+import imageio
+from PIL import Image, ImageTk
+
+def play_video(path="t.mp4", fps=60):
+    video_name = path #This is your video file path
+    video = imageio.get_reader(video_name)
+
+    def stream(label, video, fps=60):
+        time.sleep(0.005)
+        for image in video.iter_data():
+            frame_image = ImageTk.PhotoImage(Image.fromarray(image))
+            label.config(image=frame_image)
+            label.image = frame_image
+            time.sleep(1/fps)
+
+    root = tk.Tk()
+    my_label = tk.Label(root)
+    my_label.pack()
+    thread = threading.Thread(target=stream, args=(my_label,video,fps), daemon=1)
+    thread.start()
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    play_video()
