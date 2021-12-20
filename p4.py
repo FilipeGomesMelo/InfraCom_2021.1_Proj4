7939ebbd860fb23cf63f26f686db582540ee2f1c
FilipeGomesMelo <61971614+FilipeGomesMelo@users.noreply.github.com>
2021-12-20 18:06:11 -0300

    Update p4.py

--- p4.py
+++ p4.py
@@ -6,6 +6,10 @@ import threading
 import os
 from datetime import datetime
 import socket
+import time
+from pygame import mixer
+import videoPlayer
+import multiprocessing
 
 class GUI:
     def __init__(self, width, height, name, target) -> None:
@@ -25,6 +29,15 @@ class GUI:
         self.target_ip = target
 
         self.createWidgets()
+        t = threading.Thread(target = self.check_updates, daemon=True)
+        t.start()
+
+        mixer.init()
+        self.audio_channel = mixer.Channel(0)
+        self.playing_audio = False
+
+        self.media_dict = dict()
+        self.media_path_dict = dict()
 
         self.status = ''
         self.conn = None
@@ -48,7 +61,9 @@ class GUI:
         self.window.bind('<Return>', self.send)
         # self.window.bind('<Shift_R>', self.update)
         self.txt_area.bind('<Configure>', self.reset_tabstop)
-        self.window.bind('<Shift_R>', self.get_file)
+        # self.window.bind('<Shift_R>', self.get_file)
+        self.window.bind('<F1>', self.pause_audio)
+        self.window.bind('<F2>', self.stop_audio)
         self.txt_area.config(background='#7CC8CB')
 
         self.txt_area.grid(column=0, row=0, columnspan=6)
@@ -81,11 +96,54 @@ class GUI:
             self.connector_f = self.conn_f
         finally:
             self.window.title(self.status)
+
+    def write_log(self, msg):
+        log_name = f"chat_log_{self.name}.txt"
+
+        # Escreve a mensagem no log
+        with open(log_name,"a",encoding="utf-8") as log:
+            log.write(msg)
     
     def send(self, event=None):
         msg = self.txt_field.get()
         if msg.replace(' ', '') == '':
             return
+        
+        if msg[:5] == "!play":
+            try:
+                type_index = msg[6:].rindex(".")
+            except ValueError: 
+                print("404 - File Not Found")
+                self.txt_field.delete(0, END)
+                return
+
+            file_format = msg[6:][type_index+1:]
+            if file_format in ["wav","mp3","ogg"] and msg[6:] in self.media_dict:
+                # Play the sound in the file. Ex: !play me.wav
+                print(f"Tocando {msg[6:]}")
+                file = self.media_dict[msg[6:]]
+                self.play_audio(file)
+            else:
+                try:
+                    # Se o arquivo não for um arquivo de áudio, tentamos abrir ele com os, só funciona se estiver no diretório que está rodando
+                    # o cliente
+                    print(f"Abrindo {msg[6:]}")
+
+                    # Se o arquivo tiver sido enviado pelo usuário, o acessa por seu diretório.
+                    # Senão, o acessa pela pasta da aplicação
+                    if file_format in ['mp4']:
+                        p = multiprocessing.Process(target= videoPlayer.play_video, args=(msg[6:],))
+                        p.start()
+                    elif msg[6:] in self.media_path_dict:
+                        os.startfile(msg[6:])
+                    else:
+                        os.startfile(os.getcwd()+'\\'+msg[6:])
+                except:
+                    # Se não conseguir, printa que arquivo não foi achado
+                    print("404 - File Not Found")
+            self.txt_field.delete(0, END)
+            return
+        
         msg = msg.replace("\\n","\n").replace(self.separation_character, "")
         msg = f"{self.separation_character}\n{self.name}: {msg}\n{datetime.now().strftime('%d/%m/%Y, %H:%M:%S')}\n"
         self.connector.sendall(bytes(msg, 'utf-8')) 
@@ -93,9 +151,17 @@ class GUI:
         self.txt_area.insert(END, msg[len(self.separation_character):])
         self.txt_field.delete(0, END)
 
+        self.write_log("\n" + msg[4:]) # Remove alguns chars desncessários
+
     def reset_tabstop(self, event):
         event.widget.configure(tabs=(event.width-8, "right"))
 
+    # thread pra checar que chama a checagem de novas mensagens em um timer
+    def check_updates(self):
+        while True:
+            time.sleep(0.5)
+            self.update()
+            
     def update(self, event=None):
         aux = []
         try:
@@ -109,42 +175,84 @@ class GUI:
         except:
             mensagens = ''.join(aux).split(self.separation_character)[1:]
             for msg in mensagens:
-                print('>>'+msg)
                 if msg[0] == "!":
                     self.recv_file(msg)
                 else:
                     msg = msg.replace("\\n","\n")
                     self.txt_area.insert(END, msg)
-            print("Todas mensagens foram carregadas")
+                    self.write_log(msg)
 
     def clear_chat(self, event=None):
         self.txt_area.delete(1.0, END)
 
+    def play_audio(self,audio):
+        # If there is an audio playing, stop it
+        try:
+            self.audio_channel.stop()
+    
+            # Play the audio
+            self.audio_channel.play(audio)
+            self.playing_audio = True
+        except KeyError:
+            print("Audio nao encontado")
+
+    def pause_audio(self, event=None):
+        # If there is not playing, return
+        if not self.audio_channel.get_busy():
+            return
+
+        if self.playing_audio: 
+            self.audio_channel.pause()
+        else:
+            self.audio_channel.unpause()
+        
+        self.playing_audio = not self.playing_audio
+
+    def stop_audio(self, event=None):
+        # If there is not playing, return
+        if not self.audio_channel.get_busy():
+            return
+
+        self.audio_channel.stop()
+
     def get_file(self, event=None):
         file_path = filedialog.askopenfilename()
+
         if file_path == '':
-            return
-        try:
-            pic = Image.open(file_path)
-            miniature_pic = pic.resize((325, (325*pic.height)//pic.width), Image.ANTIALIAS)
-            my_img = ImageTk.PhotoImage(miniature_pic)
-            self.miniature_pics.append(my_img)
-            self.txt_area.insert(END, f'\n\t{self.name}:\n\t')
-            self.txt_area.image_create(END, image=self.miniature_pics[-1])
-            self.txt_area.insert(END, f"\n\t{datetime.now().strftime('%d/%m/%Y, %H:%M:%S')}\n")
-        except:
-            pass
+                return
+        type_index = file_path.rindex(".")
+        file_format = file_path[type_index+1:]
+        self.txt_area.insert(END, f'\n\t{self.name}:\n\t')
+        if file_format in ["wav","mp3","ogg"]:
+            audio = mixer.Sound(file_path)
+            self.media_dict[file_path] = audio
+            
+            self.txt_area.insert(END, f"#Audio: {file_path.split('/')[-1]}")
+        elif file_format in ["mp4"]:
+            self.txt_area.insert(END, f"#Video: {file_path.split('/')[-1]}")
+        else:
+            try:
+                pic = Image.open(file_path)
+                miniature_pic = pic.resize((325, (325*pic.height)//pic.width), Image.ANTIALIAS)
+                my_img = ImageTk.PhotoImage(miniature_pic)
+                self.miniature_pics.append(my_img)
+                
+                self.txt_area.image_create(END, image=self.miniature_pics[-1])
+                
+            except:
+                self.txt_area.insert(END, f"#File: {file_path.split('/')[-1]}")
+
+        self.txt_area.insert(END, f"\n\t{datetime.now().strftime('%d/%m/%Y, %H:%M:%S')}\n")
         t = threading.Thread(target = lambda: self.send_file(file_path))
         t.start()
             
     def send_file(self, file_path):
         self.send_lock.acquire()
+        print(f"Começando a enviar {file_path.split('/')[-1]}")
         size_bytes = os.path.getsize(file_path)
-        print(size_bytes)
         name = file_path.split('/')[-1].replace(';','')
         header = (self.separation_character+'!'+name+';'+str(size_bytes)+';'+
                   self.name+';'+datetime.now().strftime('%d/%m/%Y, %H:%M:%S'))
-        print(header)
         self.connector.sendall(bytes(header, 'utf-8'))
 
         file = open(file_path, 'rb')
@@ -153,7 +261,17 @@ class GUI:
             self.connector_f.sendall(l)
             l = file.read(1024)
         file.close()
-        print(f"fineshed sending {name}")
+        
+        type_index = file_path.rindex(".")
+        file_format = file_path[type_index+1:]
+
+        if file_format in ["wav","mp3","ogg"]:
+            audio = mixer.Sound(file_path)
+            self.media_dict[name] = audio
+        else:
+            self.media_path_dict[name] = file_path
+
+        print(f"Terminamos colocar {file_path.split('/')[-1]} no buffer")
         self.send_lock.release()
         
 
@@ -171,17 +289,33 @@ class GUI:
         file.write(l)
         print(f"File {name} downloaded")
         file_path = file.name
+        #file_formmat = file_path[-3:]
         file.close()
-        try:
-            pic = Image.open(file_path)
-            miniature_pic = pic.resize((325, (325*pic.height)//pic.width), Image.ANTIALIAS)
-            my_img = ImageTk.PhotoImage(miniature_pic)
-            self.miniature_pics.append(my_img)
-            self.txt_area.insert(END, f'\n{sender}:\n')
-            self.txt_area.image_create(END, image=self.miniature_pics[-1])
-            self.txt_area.insert(END, f"\n{time}\n")
-        except:
-            pass
+
+        type_index = file_path.rindex(".")
+        file_format = file_path[type_index+1:]
+        self.txt_area.insert(END, f'\n{sender}:\n')
+        if file_format in ["wav","mp3","ogg"]:
+            audio = mixer.Sound(file_path)
+            self.media_dict[file_path] = audio
+            
+            self.txt_area.insert(END, f'#Audio: {name}')
+
+        elif file_format in ["mp4"]:
+            self.txt_area.insert(END, f'#Video: {name}')
+        else:
+            try:
+                pic = Image.open(file_path)
+                miniature_pic = pic.resize((325, (325*pic.height)//pic.width), Image.ANTIALIAS)
+                my_img = ImageTk.PhotoImage(miniature_pic)
+                self.miniature_pics.append(my_img)
+                
+                self.txt_area.image_create(END, image=self.miniature_pics[-1])
+                
+            except:
+                self.txt_area.insert(END, f'#File: {name}')
+
+        self.txt_area.insert(END, f"\n{time}\n")
 
     def start(self):
         self.window.mainloop()
